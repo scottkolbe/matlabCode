04529491742af30ebbe6d6804ee73d3e142efb45
scottkolbe <kolbes@unimelb.edu.au>
2014-12-16 17:34:48 +1100

    updated GWAS_workflow/findLesionsGausMM.m

--- GWAS_workflow/findLesionsGausMM.m
+++ GWAS_workflow/findLesionsGausMM.m
@@ -6,12 +6,12 @@ path(path, [FSdir '/matlab']);
 img = MRIread('fast_flair_seg_0_values.nii.gz');
 voxVals = reshape(img.vol, size(img.vol, 1)*size(img.vol, 2)*size(img.vol, 3), 1);
 Y = sort(voxVals(voxVals > 5));
-S.mu = [Y(round(length(Y)*0.25)) Y(round(length(Y)*0.75))];
-S.sigma = [50 50];
-S.PComponents = [0.5 0.5];
+s = zeros(length(Y), 1);
+s(round(length(Y)*0.75):end) = 1;
+s = s+1;
 
 for ii = 1:100
-    obj = gmdistribution.fit(Y,2);
+    obj = gmdistribution.fit(Y,2,'Start',s);
     [~,I] = min(obj.mu);
     threshold(ii) = Y(round(length(Y)*obj.PComponents(I)));
 end
