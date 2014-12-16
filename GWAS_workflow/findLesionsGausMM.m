2e7efdc330d699712c2205c442fc399d1c6ede8f
scottkolbe <kolbes@unimelb.edu.au>
2014-12-16 17:04:02 +1100

    corrected image input to findLesionsGausMM.m

--- GWAS_workflow/findLesionsGausMM.m
+++ GWAS_workflow/findLesionsGausMM.m
@@ -3,7 +3,7 @@ warning('off', 'all');
 FSdir = getenv('FREESURFER_HOME');
 path(path, [FSdir '/matlab']);
 
-img = MRIread('flair_seg_brain_values.nii.gz');
+img = MRIread('fast_flair_seg_0_values.nii.gz');
 voxVals = reshape(img.vol, size(img.vol, 1)*size(img.vol, 2)*size(img.vol, 3), 1);
 Y = sort(voxVals(voxVals > 5));
 S.mu = [Y(round(length(Y)*0.25)) Y(round(length(Y)*0.75))];
